56097bffb2372e72860adf627b3cdca0d7b4107a
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2024-08-06 20:22:22 -0500

    upload base

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bloxr
-doxr's stupid blog, made from scratch
+doxr's blog, made from scratch (kinda)
+
+random thoughts and other things go on there.
---
+++ sitedata.js
@@ -0,0 +1,12 @@
+window.siteData = {
+    "1": {
+        "title": "Hello World!",
+        "preview": "Testing Docsify and the blog in general.",
+        "index": 1
+    },
+    "2": {
+        "title": "The Near Perfect Content Blocker?",
+        "preview": "My ideas on how to make a new extension content blocker, better than the others.",
+        "index": 2
+    }
+}
