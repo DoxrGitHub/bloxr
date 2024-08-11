29648f77d2183937254c8db0781c2dee26375443
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2024-08-11 00:03:07 -0500

    xfinity thing

--- sitedata.js
+++ sitedata.js
@@ -8,5 +8,10 @@ window.siteData = {
         "title": "The Near Perfect Content Blocker?",
         "preview": "My ideas on how to make a new extension content blocker, better than the others.",
         "index": 2
-    }
+    },
+    "3": {
+        "title": "Avoiding data usage while hosting things.",
+        "preview": "Stuff about how to use your local Xfinity public hotspot for avoiding legal issues and data usage.",
+        "index": 2
+    },
 }
